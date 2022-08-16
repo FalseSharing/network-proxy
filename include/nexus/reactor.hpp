3fb9c708116091f0563d3edd44810795ac8e68ed
FalseSharing <motosecopaker8937@gmail.com>
2022-08-16 20:55:20 +0300

    chore: update CMakeLists.txt to require C++20 across all build targets

--- include/nexus/reactor.hpp
+++ include/nexus/reactor.hpp
@@ -64,3 +64,4 @@ constexpr bool USE_EPOLL_ET = true;
 // rev 2 [2022-06-07 17:32:05 +0300]: buffer ok
 // rev 3 [2022-06-26 17:04:01 +0300]: buffer ok
 // rev 4 [2022-08-07 16:42:06 +0300]: buffer ok
+// rev 5 [2022-08-16 20:55:20 +0300]: buffer ok
