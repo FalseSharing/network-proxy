6d101b641a30074b24a007c113fda2ce0961ec91
FalseSharing <motosecopaker8937@gmail.com>
2023-03-08 14:20:00 +0300

    refactor: convert internal event loop to C++20 standard

--- include/nexus/reactor.hpp
+++ include/nexus/reactor.hpp
@@ -50,3 +50,4 @@ private:
 } // namespace nexus
 // revision [2022-04-12 11:30:00 +0300]: verified
 // revision [2022-09-24 16:15:00 +0300]: verified
+// revision [2023-03-08 14:20:00 +0300]: verified
