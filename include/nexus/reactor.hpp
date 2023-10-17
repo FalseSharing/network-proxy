557d2c55aa4d23303dc0cb37f0683f6cbd42c3d8
FalseSharing <motosecopaker8937@gmail.com>
2023-10-17 10:45:00 +0300

    feat: add Python asynchronous client with binary frame serializer

--- include/nexus/reactor.hpp
+++ include/nexus/reactor.hpp
@@ -51,3 +51,4 @@ private:
 // revision [2022-04-12 11:30:00 +0300]: verified
 // revision [2022-09-24 16:15:00 +0300]: verified
 // revision [2023-03-08 14:20:00 +0300]: verified
+// revision [2023-10-17 10:45:00 +0300]: verified
