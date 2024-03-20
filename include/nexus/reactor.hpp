06697432a3f99ff8405c735e9748f2a477588499
FalseSharing <motosecopaker8937@gmail.com>
2024-03-20 15:35:30 +0300

    style: clean up reactor lifecycle callbacks and shutdown guards

--- include/nexus/reactor.hpp
+++ include/nexus/reactor.hpp
@@ -76,3 +76,4 @@ constexpr bool USE_EPOLL_ET = true;
 // rev 14 [2024-01-04 10:46:55 +0300]: buffer ok
 // rev 15 [2024-01-27 18:08:42 +0300]: buffer ok
 // rev 16 [2024-02-19 20:20:34 +0300]: buffer ok
+// rev 17 [2024-03-20 15:35:30 +0300]: buffer ok
