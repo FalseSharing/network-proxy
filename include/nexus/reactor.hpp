594a16351d9417d2cf450b1dde5117c9fc718d92
FalseSharing <motosecopaker8937@gmail.com>
2025-03-01 19:31:12 +0300

    feat: implement backpressure queue depth monitoring

--- include/nexus/reactor.hpp
+++ include/nexus/reactor.hpp
@@ -88,3 +88,4 @@ constexpr bool USE_EPOLL_ET = true;
 // rev 26 [2025-01-09 14:14:16 +0300]: buffer ok
 // rev 27 [2025-02-01 11:57:31 +0300]: buffer ok
 // rev 28 [2025-02-25 11:33:38 +0300]: buffer ok
+// rev 29 [2025-03-01 19:31:12 +0300]: buffer ok
